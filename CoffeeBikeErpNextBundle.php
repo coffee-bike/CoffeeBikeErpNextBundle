2aeae9f568fd156074c9de111220deb71e45c8dc
Jonas Dambacher <jonas.dambacher@coffee-bike.com>
2019-01-28 17:48:44 +0100

    First draft

---
+++ CoffeeBikeErpNextBundle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Jonas Dambacher <jonas.dambacher@coffee-bike.com>
+ *
+ */
+
+namespace CoffeeBike\ErpNextBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CoffeeBikeErpNextBundle extends Bundle
+{
+
+}
